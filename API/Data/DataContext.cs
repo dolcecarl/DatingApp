c57c808ba064a4a656d8a7e3a7b1dcbe00ce5618
carl dolce <dolcecarl@hotmail.com>
2023-05-08 12:08:01 -0400

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
